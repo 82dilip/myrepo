9e47ae182e53391e7339b10cc6326a6757bb7ec0
82dilip <82.dilip@gmail.com>
2016-02-16 18:24:40 +0530

    initial commit

---
+++ src/main/java/com/coderevisited/jpa/Employee.java
@@ -0,0 +1,70 @@
+package com.coderevisited.jpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "Employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "fistName", unique=true)
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "dept")
+    private String dept;
+
+    public Employee() {
+
+    }
+
+    public Employee(String firstName, String lastName, String dept) {
+        
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setDept(dept);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+}
---
+++ src/main/java/com/coderevisited/jpa/EmployeeTest.java
@@ -0,0 +1,29 @@
+package com.coderevisited.jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EmployeeTest {
+
+    private static EntityManager em;
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("EmployeeService");
+        em = emf.createEntityManager();
+
+        createEmployee("Saint", "Peter", "Engineering");
+        createEmployee("Jack", " Dorsey", "Imaginea");
+        createEmployee("Sam", "Fox", "Imaginea");
+
+
+    }
+
+    private static void createEmployee(String firstName, String lastName, String dept) {
+        em.getTransaction().begin();
+        Employee emp = new Employee(firstName, lastName, dept);
+        em.persist(emp);
+        em.getTransaction().commit();
+    }
+}
